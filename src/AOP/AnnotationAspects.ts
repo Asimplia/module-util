2616d6b2d97ba853ddd01facccd779dd569ba023
Michael Zabka <zabka.michael@gmail.com>
2015-01-28 21:34:44 +0100

    Little refactor WET to self private method.
    * should be outside of service to self helper function

--- src/AOP/AnnotationAspects.ts
+++ src/AOP/AnnotationAspects.ts
@@ -3,6 +3,7 @@ import IAspect = require('./IAspect');
 import AspectInterception = require('./AspectInterception');
 import DependencyInjection = require('../DI/DependencyInjection');
 import _ = require('underscore');
+import fs = require('fs');
 var Finder = require('fs-finder');
 
 export = AnnotationAspects;
@@ -11,37 +12,52 @@ class AnnotationAspects implements IAspect {
 	static ANNOTATION_ASPECT = '$aspect';
 	static FILES_PATTERN = '*.js';
 
+	private paths: string[];
+
 	static $args = [null];
 	static $inject = [AspectInterception, DependencyInjection];
 	constructor(
-		private paths: string|string[],
+		paths: string|string[],
 		private aspectInterception: AspectInterception,
 		private dependencyInjection: DependencyInjection
 	) {
 		if (typeof paths === 'string') {
 			this.paths = [paths];
+		} else {
+			this.paths = paths;
 		}
 		this.aspectInterception.addAspect('AOP.AnnotationAspects', this);
 	}
 
 	intercept() {
-		var files = _.flatten(_.map(this.paths, (path: string) => {
-			return Finder.from(path).findFiles(AnnotationAspects.FILES_PATTERN);
-		}));
-		files.forEach((file: string) => {
-			var Static = require(file);
+		var classes = this.getClasses();
+		classes.forEach((Static: any) => {
 			if (typeof Static !== 'function') {
 				return;
 			}
 			var aspectName = Static[AnnotationAspects.ANNOTATION_ASPECT];
-			if (typeof aspectName !== 'string') {
+			if (typeof aspectName === 'undefined') {
 				return;
+			}
+			if (typeof aspectName !== 'string') {
+				throw new Error(AnnotationAspects.ANNOTATION_ASPECT + ' annotation must be string ' + aspectName + ' given.');
 			}
 			if (!this.dependencyInjection.hasService(Static)) {
 				throw new Error('Aspect annotated with $aspect needs to be a service in DependencyInjection container');
 			}
 			var aspect = this.dependencyInjection.get<any>(Static);
 			this.aspectInterception.addAspect(aspectName, aspect);
+		});
+	}
+
+	private getClasses() {
+		return _.map(_.flatten(_.map(this.paths, (path: string) => {
+			if (fs.lstatSync(path).isFile()) { // TODO check pattern FILES_PATTERN
+				return path;
+			}
+			return Finder.from(path).findFiles(AnnotationAspects.FILES_PATTERN);
+		})), (file: string) => {
+			return require(file);
 		});
 	}
 }
