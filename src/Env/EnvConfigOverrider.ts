a173a823ee85e704e86a8431ad37c2bce1d64ce3
Michael Zabka <zabka.michael@gmail.com>
2015-02-03 23:03:20 +0100

    Add aspect of .env.json override empty env variables to process.env

---
+++ src/Env/EnvConfigOverrider.ts
@@ -0,0 +1,25 @@
+
+import _ = require('underscore');
+import fs = require('fs');
+
+export = EnvConfigOverrider;
+class EnvConfigOverrider {
+	
+	static $aspect = 'Env.EnvConfigOverrider';
+	static $service = 'Env.EnvConfigOverrider';
+	static $args = [null];
+	constructor(
+		private basePath: string
+	) {}
+
+	intercept() {
+		if (!fs.existsSync(this.basePath)) {
+			console.warn('Base directory of .env.json configuration not found: ' + this.basePath);
+		}
+		// override of EnvVar using file
+		var envFilePath = this.basePath + '/.env.json';
+		if (fs.existsSync(envFilePath)) {
+			process.env = _.defaults({}, process.env, require(envFilePath));
+		}
+	}
+}
---
+++ tests/unit/Env/EnvConfigOverriderSpec.ts
@@ -0,0 +1,34 @@
+
+import Util = require('../../../src/index');
+import EnvConfigOverrider = Util.Env.EnvConfigOverrider;
+
+describe('Env.EnvConfigOverrider', () => {
+
+	it('should override process.env variables from .env.json in specified basePath if empty', () => {
+		delete process.env.OVERRODE_VAR;
+		var envConfigOverrider = new EnvConfigOverrider(__dirname + '/../../../../src/Env/fixtures');
+		envConfigOverrider.intercept();
+		expect(process.env.OVERRODE_VAR).toBe('is not foo');
+	});
+
+	it('should override process.env variables from .env.json in specified basePath if undefined', () => {
+		process.env.OVERRODE_VAR = undefined;
+		var envConfigOverrider = new EnvConfigOverrider(__dirname + '/../../../../src/Env/fixtures');
+		envConfigOverrider.intercept();
+		expect(process.env.OVERRODE_VAR).toBe('is not foo');
+	});
+
+	it('should not override process.env variables from .env.json if specified already', () => {
+		process.env.OVERRODE_VAR = 'foo';
+		var envConfigOverrider = new EnvConfigOverrider(__dirname + '/../../../../src/Env/fixtures');
+		envConfigOverrider.intercept();
+		expect(process.env.OVERRODE_VAR).toBe('foo');
+	});
+
+	it('should not override process.env variables from .env.json if null. null is correct value', () => {
+		process.env.OVERRODE_VAR = null;
+		var envConfigOverrider = new EnvConfigOverrider(__dirname + '/../../../../src/Env/fixtures');
+		envConfigOverrider.intercept();
+		expect(process.env.OVERRODE_VAR).toBeNull();
+	});
+});
