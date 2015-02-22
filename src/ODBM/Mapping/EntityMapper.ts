1d088bfbed4308425f46ea72ea38a7ee2d2f7829
Michael Zabka <zabka.michael@gmail.com>
2015-02-22 19:02:12 +0100

    Move annotation normalizing to self class

--- src/ODBM/Mapping/EntityMapper.ts
+++ src/ODBM/Mapping/EntityMapper.ts
@@ -6,6 +6,7 @@ import Type = require('./Annotation/Type');
 import ITypeStatic = require('./Annotation/ITypeStatic');
 import IPropertyAnnotation = require('../Entity/Annotation/IPropertyAnnotation');
 import DatabaseSystem = require('../DBS/DatabaseSystem');
+import AnnotationNormalizer = require('./AnnotationNormalizer');
 
 export = EntityMapper;
 class EntityMapper<Entity, EntityObject> {
@@ -13,56 +14,8 @@ class EntityMapper<Entity, EntityObject> {
 	constructor(
 		private EntityStatic: IEntityStatic<Entity, EntityObject>
 	) {
-		this.normalizeEntityAnnotations();
-		this.normalizePropertyAnnotations();
-	}
-
-	private normalizeEntityAnnotations() {
-		var entityAnnotation = this.EntityStatic.$entity;
-		if (typeof entityAnnotation.$name === 'undefined') {
-			entityAnnotation.$name = (<any>this.EntityStatic).name; // get class name (function name of ES5)
-		}
-		if (typeof entityAnnotation.$name !== 'string') {
-			throw new Error('Entity annotation $name must be string');
-		}
-		if (typeof entityAnnotation.$object === 'undefined') {
-			entityAnnotation.$object = 'object';
-		}
-		if (typeof entityAnnotation.$object !== 'string') {
-			throw new Error('Entity annotation $object must be string');
-		}
-		var testObject = <any>{ $$testProperty: true };
-		var testEntity = new this.EntityStatic(testObject);
-		if (testEntity[entityAnnotation.$object] !== testObject) {
-			throw new Error('Object passed by constructor must be accessible by propertyName from annotation $object');
-		}
-	}
-
-	private normalizePropertyAnnotations() {
-		var entityAnnotation = this.EntityStatic.$entity;
-		var keys = this.getKeys();
-		_.forEach(keys, (key: string) => {
-			var propertyAnnotation = <IPropertyAnnotation>entityAnnotation[key];
-			if (propertyAnnotation instanceof Type || this.isTypeStatic(propertyAnnotation)) {
-				propertyAnnotation = <IPropertyAnnotation><any>{ $type: propertyAnnotation };
-			}
-			if (!(propertyAnnotation.$type instanceof Type)) {
-				propertyAnnotation.$type = new (<ITypeStatic>propertyAnnotation.$type)();
-			}
-			if (!(propertyAnnotation.$type instanceof Type)) {
-				throw new Error('Type must be instance of Type or constructor of Type');
-			}
-			if (typeof propertyAnnotation.$name === 'undefined') {
-				propertyAnnotation.$name = key;
-			}
-		});
-	}
-
-	private isTypeStatic(
-		propertyAnnotation: IPropertyAnnotation|Type|ITypeStatic|DatabaseSystem|string
-	) {
-		return typeof propertyAnnotation === 'function'
-			&& (new (<ITypeStatic>propertyAnnotation)()) instanceof Type;
+		var annotationNormalizer = new AnnotationNormalizer<Entity, EntityObject>(EntityStatic);
+		annotationNormalizer.normalizeEntityAnnotations();
 	}
 
 	getIdKey(): string {
