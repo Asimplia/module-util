0f7c2390030245b67c0823f4b05e64d4b63c2bd4
Michael Zabka <zabka.michael@gmail.com>
2015-02-23 11:05:04 +0100

    Add unit test of mongodb model builder

---
+++ tests/unit/ODBM/Repository/MongoDB/ModelBuilderSpec.ts
@@ -0,0 +1,24 @@
+
+import ModelBuilder = require('../../../../../src/ODBM/Repository/MongoDB/ModelBuilder');
+import EntityMapper = require('../../../../../src/ODBM/Mapping/EntityMapper');
+import My = require('../../../fixtures/My');
+import MyEntity = My.MyEntity;
+import MyEntityObject = My.MyEntityObject;
+
+describe('ODBM.Repository.MongoDB.ModelBuilder', () => {
+	var entityMapper = new EntityMapper<MyEntity, MyEntityObject>(MyEntity);
+	var modelBuilder = new ModelBuilder<MyEntity, MyEntityObject>(entityMapper);
+
+	describe('getEmbeddedDefinition', () => {
+		var definition = (<any>modelBuilder).getEmbeddedDefinition([]); // test private, becouse create is depend on mongoose
+		expect(definition).toEqual({
+			id: Number,
+			entity_name: String,
+			embedded: {
+				description: String,
+				created_at: Date
+			}
+		});
+	});
+
+});
