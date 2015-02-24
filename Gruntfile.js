b9b9bb8f5c4f86b8d402ac64977ba9821fd5417f
Michael Zabka <zabka.michael@gmail.com>
2015-02-24 12:11:10 +0100

    Add clean grunt for easy clean build on compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,12 @@ module.exports = function (grunt) {
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-typescript');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-jasmine-node');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-contrib-watch');
+	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-contrib-clean');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-tsd');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-wait');
 
 	grunt.registerTask('default', [
-		'tsd:reinstall', 'wait:typings', 'typescript:build', 'jasmine_node:unit'
+		'clean:build', 'tsd:reinstall', 'wait:typings', 'typescript:build', 'jasmine_node:unit'
 	]);
 	grunt.registerTask('dev', function () {
 		GruntConfiguration.typescriptReferences(__dirname + '/build/references.ts', [
