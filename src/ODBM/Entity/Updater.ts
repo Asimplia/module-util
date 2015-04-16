3e7eaf33f8dc02131dfcd5634417ffab6831a8f2
Michael Zabka <zabka.michael@gmail.com>
2015-04-16 20:50:28 +0200

    Updater convert values to right type on set/get

--- src/ODBM/Entity/Updater.ts
+++ src/ODBM/Entity/Updater.ts
@@ -1,23 +1,30 @@
 
 import EntityMapper = require('../Mapping/EntityMapper');
+import PropertyConverter = require('./Property/Converter');
 
 export = Updater;
 class Updater<Entity, EntityObject> {
 
+	private propertyConverter: PropertyConverter;
+
 	constructor(
 		private entityMapper: EntityMapper<Entity, EntityObject>
-	) {}
+	) {
+		this.propertyConverter = new PropertyConverter();
+	}
 
-	set(entity: Entity, key: string, value: any) {// TODO check by PropertyConverter
+	set(entity: Entity, key: string, value: any) {
+		var propertyType = this.entityMapper.getPropertyTypeByKey(key);
 		var objectPropertyName = this.entityMapper.getObjectPropertyName();
 		var object = entity[objectPropertyName];
-		object[key] = value;
+		object[key] = this.propertyConverter.convertByType(propertyType, value);
 		return this;
 	}
 
 	get(entity: Entity, key: string): any {
+		var propertyType = this.entityMapper.getPropertyTypeByKey(key);
 		var objectPropertyName = this.entityMapper.getObjectPropertyName();
 		var object = entity[objectPropertyName];
-		return object[key];
+		return this.propertyConverter.convertByType(propertyType, object[key]);
 	}
 }
