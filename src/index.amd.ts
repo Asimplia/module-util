b83e844cf40d1631395d10c3f711609113863d45
Michael Zabka <zabka.michael@gmail.com>
2015-05-19 22:56:21 +0200

    Delete cache from amd

--- src/index.amd.ts
+++ src/index.amd.ts
@@ -2,11 +2,9 @@
 export import Collection = require('./Collection/index');
 export import DateTime = require('./DateTime/index');
 export import ODBM = require('./ODBM/index.amd');
-export import Cache = require('./Cache/index');
 export import Error = require('./Error/index');
 /* tslint:disable */
 Collection;
 DateTime;
 ODBM;
-Cache;
 Error;
